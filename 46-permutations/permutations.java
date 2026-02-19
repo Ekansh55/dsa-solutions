524120acff1c98331352c2dc93c102fba9bc4514
Ekansh55 <sheetupruthi6126@gmail.com>
2026-02-19 13:34:58 +0530

    Time: 0 ms (100.00%) | Memory: 45.5 MB (64.07%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,23 +1,31 @@
 class Solution {
-    private void helper(int nums[],boolean used[],List<List<Integer>>ans,List<Integer>curr){
-        if(curr.size()==nums.length){
-            ans.add(new ArrayList<>(curr));
+    public void helper(int[] nums,int idx,List<List<Integer>> ans ){
+        int n = nums.length;
+        if(idx==n-1){
+            List<Integer> l = new ArrayList<>();
+            for(int i=0;i<n;i++){
+                l.add(nums[i]);
+            }
+            ans.add(l);
             return;
+
         }
-        for(int i=0;i<nums.length;i++){
-            if(used[i]) continue;
-            curr.add(nums[i]);
-            used[i]=true;
-            helper(nums,used,ans,curr);
-            curr.remove(curr.size()-1);
-            used[i]=false;
+        for(int i=idx;i<n;i++){
+            swap(i,idx,nums);
+            helper(nums,idx+1,ans);
+            swap(i,idx,nums);
         }
+
+    }
+    public void swap(int i,int j,int[] nums){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>>ans=new ArrayList<>();
-        boolean used[]=new boolean[nums.length];
-        helper(nums,used,ans,new ArrayList<>());
-        return ans;
-        
+        List<List<Integer>> ans = new ArrayList<>();
+        helper(nums,0,ans);
+        return ans; 
+
     }
 }

073ecff21b8826901cd8431caa8c3c177635bdf6
Ekansh55 <sheetupruthi6126@gmail.com>
2026-09-10 14:40:08 +0530

    Time: 12 ms (11.45%) | Memory: 43.5 MB (8.35%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -1,16 +1,25 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        int[] freq = new int[26];
-        
-        for(char ch:s.toCharArray()){
-            freq[ch- 'a']++;
-        }
-        for(char ch:t.toCharArray()){
-            freq[ch- 'a']--;
-        
 
-            if(freq[ch- 'a']<0) return ch;
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for(char ch : s.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
+
+        for(char ch : t.toCharArray()) {
+
+            if(!map.containsKey(ch)) {
+                return ch;
+            }
+
+            map.put(ch, map.get(ch) - 1);
+
+            if(map.get(ch) < 0) {
+                return ch;
+            }
+        }
+
         return ' ';
     }
 }
